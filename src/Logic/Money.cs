7feb8770bcc5e5cbdc74989d599b76e64351dfea
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-08-17 15:24:43 +0100

    add SnackMachine entity

---
+++ src/Logic/Money.cs
@@ -0,0 +1,33 @@
+﻿namespace Logic;
+
+public sealed class Money
+{
+    public int OneCentCount { get; private set; }
+    public int TenCentCount { get; private set; }
+    public int QuarterCentCount { get; private set; }
+    public int OneDollarCount { get; private set; }
+    public int FiveDollarCount { get; private set; }
+    public int TwentyDollarCount { get; private set; }
+
+    public Money(int oneCentCount, int tenCentCount, int quarterCentCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+    {
+        OneCentCount = oneCentCount;
+        TenCentCount = tenCentCount;
+        QuarterCentCount = quarterCentCount;
+        OneDollarCount = oneDollarCount;
+        FiveDollarCount = fiveDollarCount;
+        TwentyDollarCount = twentyDollarCount;
+    }
+
+    public static Money operator +(Money first, Money second)
+    {
+        return new Money(
+            oneCentCount: first.OneCentCount + second.OneCentCount,
+            tenCentCount: first.TenCentCount + second.TenCentCount,
+            quarterCentCount: first.QuarterCentCount + second.QuarterCentCount,
+            oneDollarCount: first.OneDollarCount + second.OneDollarCount,
+            fiveDollarCount: first.FiveDollarCount + second.FiveDollarCount,
+            twentyDollarCount: first.TwentyDollarCount + second.TwentyDollarCount
+            );
+    }
+}
--- src/Logic/SnackMachine.cs
+++ src/Logic/SnackMachine.cs
@@ -1,6 +1,27 @@
 ﻿namespace Logic;
 
-public sealed class SnackMachine
+public sealed class SnackMachine : Entity
 {
+    public Money MoneyInside { get; private set; } /* This money belongs to the machine */
+    public Money MoneyInTrasantion { get; private set; } /* This money represents the current transaction */
 
+    public void InsertMoney(Money money)
+    {
+        MoneyInTrasantion += money;
+    }
+
+    /* To cancel the current transaction */
+
+    public void ReturnMoney()
+    {
+        //MoneyInTrasantion = 0;
+    }
+
+    public void BuySnack()
+    {
+        // put the money inside the machine
+        MoneyInside += MoneyInTrasantion;
+
+        ReturnMoney();
+    }
 }
