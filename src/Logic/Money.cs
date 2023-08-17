edd10f930a743cc04e36e8ad274dc92108d2c47c
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-08-17 17:03:36 +0100

    building money class

--- src/Logic/Money.cs
+++ src/Logic/Money.cs
@@ -2,15 +2,36 @@
 
 public sealed class Money : ValueObject<Money>
 {
-    public int OneCentCount { get; private set; }
-    public int TenCentCount { get; private set; }
-    public int QuarterCentCount { get; private set; }
-    public int OneDollarCount { get; private set; }
-    public int FiveDollarCount { get; private set; }
-    public int TwentyDollarCount { get; private set; }
+    public int OneCentCount { get; }
+    public int TenCentCount { get; }
+    public int QuarterCentCount { get; }
+    public int OneDollarCount { get; }
+    public int FiveDollarCount { get; }
+    public int TwentyDollarCount { get; }
+
+    public decimal Amount
+    {
+        get
+        {
+            return OneCentCount * 0.01m + TenCentCount * 0.1m + QuarterCentCount * 0.25m + OneDollarCount + FiveDollarCount * 5 + TwentyDollarCount * 20;
+        }
+    }
 
     public Money(int oneCentCount, int tenCentCount, int quarterCentCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
     {
+        if (oneCentCount < 0)
+            throw new InvalidOperationException();
+        if (tenCentCount < 0)
+            throw new InvalidOperationException();
+        if (quarterCentCount < 0)
+            throw new InvalidOperationException();
+        if (oneDollarCount < 0)
+            throw new InvalidOperationException();
+        if (fiveDollarCount < 0)
+            throw new InvalidOperationException();
+        if (twentyDollarCount < 0)
+            throw new InvalidOperationException();
+
         OneCentCount = oneCentCount;
         TenCentCount = tenCentCount;
         QuarterCentCount = quarterCentCount;
@@ -28,6 +49,18 @@ public sealed class Money : ValueObject<Money>
             oneDollarCount: first.OneDollarCount + second.OneDollarCount,
             fiveDollarCount: first.FiveDollarCount + second.FiveDollarCount,
             twentyDollarCount: first.TwentyDollarCount + second.TwentyDollarCount
+            );
+    }
+
+    public static Money operator -(Money first, Money second)
+    {
+        return new Money(
+            oneCentCount: first.OneCentCount - second.OneCentCount,
+            tenCentCount: first.TenCentCount - second.TenCentCount,
+            quarterCentCount: first.QuarterCentCount - second.QuarterCentCount,
+            oneDollarCount: first.OneDollarCount - second.OneDollarCount,
+            fiveDollarCount: first.FiveDollarCount - second.FiveDollarCount,
+            twentyDollarCount: first.TwentyDollarCount - second.TwentyDollarCount
             );
     }
 
