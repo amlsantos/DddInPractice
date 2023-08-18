8bd1b3d6d9adbb18d5559bbbc341b2d4ec9655c2
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-08-18 22:15:26 +0100

    working ui version

---
+++ src/UI/Common/CustomWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace UI.Common;
+
+public partial class CustomWindow : Window
+{
+    public CustomWindow(ViewModel viewModel)
+    {
+        InitializeComponent();
+
+        DataContext = viewModel;
+    }
+}
---
+++ src/UI/Services/DialogService.cs
@@ -0,0 +1,14 @@
+﻿using UI.Common;
+
+namespace UI.Services;
+
+public class DialogService
+{
+    public bool? ShowDialog(ViewModel viewModel)
+    {
+        //CustomWindow window = new CustomWindow(viewModel);
+        //return window.ShowDialog();
+
+        return true;
+    }
+}
