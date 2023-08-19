d56841f57b879b89e584b0b08734c5e3f6a32590
Andre Santos <6472330+santos-an@users.noreply.github.com>
2023-08-19 12:05:11 +0100

    refactor data template

--- src/UI/Common/MainDataTemplateSelector.cs
+++ src/UI/Common/MainDataTemplateSelector.cs
@@ -9,17 +9,13 @@ public class MainDataTemplateSelector : DataTemplateSelector
     public override DataTemplate SelectTemplate(object item, DependencyObject container)
     {
         if (item == null || Application.Current == null)
-        {
             return null;
-        }
 
-        string name = item.GetType().Name;
-        DataTemplate template = (DataTemplate)Application.Current.TryFindResource(name);
+        var name = item.GetType().Name;
+        var template = (DataTemplate)Application.Current.TryFindResource(name);
 
         if (template == null)
-        {
             throw new ArgumentException("Template for ViewModel " + name + " was not found");
-        }
 
         return template;
     }
